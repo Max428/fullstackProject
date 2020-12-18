1f8213f487d0b052895e6fe7d35e7022adcd551f
Max428 <55417346+Max428@users.noreply.github.com>
2020-12-18 06:33:07 +0100

    added share button

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,8 +1,10 @@
 import React from "react";
+import SharePortfolioButton from '../components/ShareButton.js';
 
 const StockSite = () => {
     return(<>
     <h1>Loppis</h1>
+    <SharePortfolioButton/>
     </>)
 }
 
---
+++ src/components/ShareButton.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+
+
+const SharePortfolioButton = ({name, price}) => {
+
+    useEffect(() => {
+        
+    }, []);
+
+
+    return(
+        <button>Dela portfölj</button>
+    )
+}
+
+export default SharePortfolioButton;
